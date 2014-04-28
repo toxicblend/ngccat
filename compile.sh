c1aa8c8c47ff9039e3021cc8a8f6a2fc87c3d172
EAD Fritz <ead.fritz@gmail.com>
2014-04-28 16:14:35 +0200

    Removed a "-n" command line option from the example

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 python ngccat.py begin rough safez 1-side1-outline.ngc safez 2-side1-rough.ngc safez end -o side1-rough-8mm-End.ngc
-python ngccat.py begin fine3 safez 3-side1-fine.ngc safez end -n -o side1-fine-8mm-Ball.ngc
+python ngccat.py begin fine3 safez 3-side1-fine.ngc safez end -o side1-fine-8mm-Ball.ngc
 
 python ngccat.py begin rough safez 5-side2-outline.ngc safez 6-side2-rough.ngc safez end -o side2-rough-8mm-End.ngc
 python ngccat.py begin fine3 safez 7-side2-fine.ngc safez safez 8-side2-cutout1.ngc safez 8-side2-cutout2.ngc safez 8-side2-cutout3.ngc safez 8-side2-cutout4.ngc end  -o side2-fine-8mm-Ball.ngc
